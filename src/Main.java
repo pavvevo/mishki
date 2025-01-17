55648209528dbde9bab03931e926395c383fde6f
xXboob1Xx <aleksadar2007@gmail.com>
2025-01-17 20:59:46 +0200

    haha

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("kiril i metodii");
         //QJ MI SOPOLITEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+        //ko staa aide da si pravim stoqnki
+        //ili ognqnki
     }
 }
