6eb5a90fb48938c6e8915ff3c524179bd061e7e0
xXboob1Xx <aleksadar2007@gmail.com>
2025-01-19 16:48:30 +0200

    map connections done

--- src/Entity/UI/Nodes/Chest.java
+++ src/Entity/UI/Nodes/Chest.java
@@ -5,6 +5,7 @@ import Main.Game;
 import java.awt.image.BufferedImage;
 
 public class Chest extends Node{
+    String name = "Chest";
     public Chest(Game game) {
         super(game);
         setSprite(getImg("/Resources/UI/Map/map_icon_chest.png"));
